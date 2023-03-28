8ce93118b659332446a486ea8abdaf89b3ff2146
QuanQiuTong <62756763+QuanQiuTong@users.noreply.github.com>
2023-03-28 23:08:49 +0800

    Update main.cpp

--- HW3_3_22307110118/main.cpp
+++ HW3_3_22307110118/main.cpp
@@ -28,37 +28,29 @@ int main()
 		v3.push_back(r_string());
 	}
 
+	// int list
 	cout << "the integer list :" << endl;
-
 	print(v1);
-
 	cout << "the sum of the integer list:" << fqt::accumulate(v1.begin(), v1.end(), int()) << endl;
-
-	cout << "the max element in the integer list:" << *fqt::max_element(v1.begin(), v1.end()) << endl; // 使用全局命名空间，这样保证是我所写的那个函数
-
+	// 最后一个参数是调用默认无参构造函数产生缺省值，可以保证类型正确。内置类型的缺省值皆为0。
+	cout << "the max element in the integer list:" << *fqt::max_element(v1.begin(), v1.end()) << endl;
 	cout << "the min element in the integer list:" << *fqt::min_element(v1.begin(), v1.end()) << endl
 		 << endl;
-	// int list
+
+	// double list
 	cout << "the double list :" << endl;
-
 	print(v2);
-
 	cout << "the sum of the double list:" << fqt::accumulate(v2.begin(), v2.end(), double()) << endl;
-
 	cout << "the max element in the double list:" << *fqt::max_element(v2.begin(), v2.end()) << endl;
-
 	cout << "the min element in the double list:" << *fqt::min_element(v2.begin(), v2.end()) << endl
 		 << endl;
-	// double list
 
+	// string list
 	cout << "the string list :" << endl;
-
 	print(v3);
-
 	cout << "the concatenation of the string list:" << fqt::accumulate(v3.begin(), v3.end(), string()) << endl;
-
 	cout << "the max element in the string list:" << *fqt::max_element(v3.begin(), v3.end()) << endl;
 	cout << "the min element in the string list:" << *fqt::min_element(v3.begin(), v3.end()) << endl;
-	// string list
+
 	return 0;
 }
